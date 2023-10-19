8dfc4eab2bb11138827ecadc503f1f3c30940b64
Eric <ericdelmermillen@gmail.com>
2023-10-19 09:20:38 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const nav = document.querySelector('.nav');
+const headerMenuToggle = document.querySelector('.header__menu-toggle');
+const navMenuToggle = document.querySelector('.nav__menu-toggle');
+
+const navOptions = document.querySelectorAll(".nav__options")
+
+headerMenuToggle.addEventListener('click',() => nav.classList.add('show') )
+
+navMenuToggle.addEventListener('click',() => nav.classList.remove('show'))
+
+navOptions.forEach(option => option.addEventListener('click',() => {
+  console.log(event.target);
+  nav.classList.remove('show')
+}))
+
+document.body.addEventListener("click", (event) => {
+  if (!nav.contains(event.target) && event.target !== headerMenuToggle && !event.target.classList.contains('nav__options')) {
+    nav.classList.remove('show');
+    console.log(event.target);
+  }
+});
+
+
+const year = document.getElementById('year')
+  
+const thisYear = new Date().getFullYear();
+
+year.innerText = thisYear;
+
