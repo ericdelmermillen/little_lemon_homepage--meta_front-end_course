9a0c5c0ff9e51a0b076b5cd51ea034a145d500d3
Eric <ericdelmermillen@gmail.com>
2023-10-22 16:07:11 +0700

    added show/hide on scroll for header at mobile

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const nav = document.querySelector('.nav');
+const header = document.querySelector('.header');
+
 const headerMenuToggle = document.querySelector('.header__menu-toggle');
 const navMenuToggle = document.querySelector('.nav__menu-toggle');
 
@@ -15,19 +17,18 @@ headerMenuToggle.addEventListener('click',() => {
 navMenuToggle.addEventListener('click',() => nav.classList.remove('show'))
 
 navOptions.forEach(option => option.addEventListener('click',() => {
-  console.log(event.target);
   nav.classList.remove('show');
 }))
 
 document.body.addEventListener("click", (event) => {
   if (!nav.contains(event.target) && event.target !== headerMenuToggle && !event.target.classList.contains('nav__options')) {
     nav.classList.remove('show');
-    console.log(event.target);
   }
 });
 
 window.onscroll = function() {
   let currentScrollPos = window.scrollY;
+  nav.style.top = "0";
   
   if(innerWidth >= 768) {
     if(prevScrollpos > currentScrollPos) {
@@ -40,9 +41,15 @@ window.onscroll = function() {
     prevScrollpos = currentScrollPos;
   } 
   else if(innerWidth < 768){
-    nav.style.top = "0";
+    if(prevScrollpos > currentScrollPos) {
+      header.style.top = "0";
+    } else {
+      header.style.top = "-6rem";
+    }
+    prevScrollpos = currentScrollPos;
   }
 }
+
 
 const year = document.getElementById('year')
   
