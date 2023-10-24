8e00331522168a9dbe03bfc5f0bf1cc4bbec7b94
Eric <ericdelmermillen@gmail.com>
2023-10-24 20:26:45 +0700

    removed unneeded comments

--- index.js
+++ index.js
@@ -20,19 +20,6 @@ window.onload = () => {
         window.scrollTo(0, 0)}, 750);
   }
 };
-      
-// if (isAndroid) {
-//   setTimeout(() => {
-//     window.scrollTo(0, 5)}, 500);
-//       setTimeout(() => {
-//         window.scrollTo(0, 0)}, 750);
-//         console.log("This is an Android device.");
-// } else {
-//   // Execute code for non-Android devices
-//   console.log("This is not an Android device.");
-// }
-
-
 
 headerMenuToggle.addEventListener('click',() => {
   nav.classList.add('show');
